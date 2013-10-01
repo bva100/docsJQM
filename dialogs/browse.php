ccc8582f105ca6b8b5db306fb253254533c1f99b
bva100 <brianvanderson@gmail.com>
2013-10-01 09:48:20 -0700

    trying no dialog

--- dialogs/browse.php
+++ dialogs/browse.php
@@ -19,11 +19,11 @@ try{
 <!DOCTYPE html>
 <html>
 <head>
+    <title>PipeStack Docs</title>
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PipeStack Developers</title>
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
 </head>
 <body>
